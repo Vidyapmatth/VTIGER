8ed3411036c381541c2a67e18be8a4d862b3ec33
LENOVO <LENOVO@DESKTOP-6RAGRLU>
2024-07-02 10:46:54 +0530

    push_FW

---
+++ Vitiger/src/main/java/Generic_Utilities/DataBaseUtility.java
@@ -0,0 +1,15 @@
+package Generic_Utilities;
+
+public class DataBaseUtility {
+	public void dataBaseConnection(WebDriver driver) throws Throwable
+	{
+	//	Driver driverRef=new Driver();
+	//	 DriverManager.registerDriver(driverRef);
+        }
+	
+	public void dataBaseClose() throws Throwable
+	{
+	//	Connection con=	DriverManager.getConnection("jdbc:mysql://localhost:3306/qspiders", "root", "root");
+	//     con.close();
+	}
+}
